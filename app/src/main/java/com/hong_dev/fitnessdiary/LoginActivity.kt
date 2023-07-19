d484c89d387a50147e0fb9eea13e9e1260455487
parkkh <parkkh220706@gmail.com>
2023-07-19 12:16:58 +0900

    login activity구현

--- app/src/main/java/com/hong_dev/fitnessdiary/LoginActivity.kt
+++ app/src/main/java/com/hong_dev/fitnessdiary/LoginActivity.kt
@@ -19,15 +19,6 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        binding.btnRegister.setOnClickListener {
-            val email = binding.etId.text.toString()
-            val password = binding.etPw.text.toString()
-
-            if(email.isNotEmpty() && password.isNotEmpty()){
-                registerUser(email, password)
-            }
-
-        }
 
         binding.btnLogin.setOnClickListener {
             val email = binding.etId.text.toString()
@@ -38,29 +29,13 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        binding.btnRegister.setOnClickListener {
+            val intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
-    private fun registerUser(email: String, password: String){
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this){task ->
-                if(task.isSuccessful){
-                    // 회원가입 성공
-                    auth.currentUser?.sendEmailVerification()?.addOnCompleteListener{sendTask ->
-                        if(sendTask.isSuccessful){
-                            // 이메일 전송 성공
-                            Toast.makeText(this, "인증 메일이 전송되었습니다.", Toast.LENGTH_SHORT).show()
-                        }else{
-                            // 이메일 전송 실패
-                            Toast.makeText(this, "인증 메일 전송에 실패하였습니다.", Toast.LENGTH_SHORT).show()
-
-                        }
-                    }
-                }else{
-                    // 회원가입 실패
-                    Toast.makeText(this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
 
     private fun loginUser(email: String, password: String){
         auth.signInWithEmailAndPassword(email, password)
